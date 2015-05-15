2313b89b45fb09170541b58e5a534523d4d82d52
Oleg Plakhotniuk <olegus8@gmail.com>
2015-05-15 17:15:52 -0500

    examples

--- hinance-www/www/examples/max/in/user_data.hs
+++ hinance-www/www/examples/max/in/user_data.hs
@@ -8,11 +8,90 @@ import Hinance.Bank.Type
 import Hinance.Shop.Type
 import Hinance.Currency
 
+blue = "#337AB7"
+cyan = "#5BC0DE"
+green = "#5CB85C"
+grey = "#777"
+red = "#D9534F"
+white = "#FFF"
+yellow = "#F0AD4E"
+
 addtagged _ = [] :: [Tag]
 canxfer _ _ = False
 canmerge _ _ = False
-tagparts = []
-slices = [] :: [Slice]
+
+slices = [
+  Slice {sname="Expenses", stags=[TagExpense], scategs=[
+    SliceCateg {scname="Rent, Car and Recurring", scbg=red, scfg=white,
+      sctags=[TagCar, TagEnergy, TagInsurance, TagPhone, TagRent,
+              TagShipping, TagTax]},
+    SliceCateg {scname="Food", scbg=yellow, scfg=white,
+      sctags=[TagFood, TagEatingOut]},
+    SliceCateg {scname="Medicine and Hygiene", scbg=green, scfg=white,
+      sctags=[TagMedSvc, TagDrugs, TagHygiene]},
+    SliceCateg {scname="Household and Electronics", scbg=cyan, scfg=white,
+      sctags=[TagElectronics, TagHousehold, TagKitchen]},
+    SliceCateg {scname="Clothes, Books, Fun and Travel", scbg=blue, scfg=white,
+      sctags=[TagBooks, TagClothes, TagGames, TagGoingOut,
+              TagHobby, TagMovies, TagTravel]},
+    SliceCateg {scname="Other", scbg=grey, scfg=white,
+      sctags=[TagOther]}]},
+
+  Slice {sname="Hobbies", stags=[TagHobby], scategs=[
+    SliceCateg {scname="Outdoor", scbg=green, scfg=white,
+      sctags=[TagOutdoor]},
+    SliceCateg {scname="Yoga", scbg=cyan, scfg=white,
+      sctags=[TagYoga]},
+    SliceCateg {scname="Weight Lifting", scbg=blue, scfg=white,
+      sctags=[TagWeight]},
+    SliceCateg {scname="Projects", scbg=yellow, scfg=white,
+      sctags=[TagProjects]},
+    SliceCateg {scname="Growing", scbg=red, scfg=white,
+      sctags=[TagGrow]}]},
+
+  Slice {sname="Car", stags=[TagCar], scategs=[
+    SliceCateg {scname="Gas", scbg=green, scfg=white,
+      sctags=[TagGas]},
+    SliceCateg {scname="Insurance and Paperwork", scbg=cyan, scfg=white,
+      sctags=[TagCarInsur, TagCarPaper]},
+    SliceCateg {scname="Parking", scbg=blue, scfg=white,
+      sctags=[TagParking]},
+    SliceCateg {scname="Maintenance", scbg=yellow, scfg=white,
+      sctags=[TagCarMtn]}]},
+
+  Slice {sname="Travel", stags=[TagTravel], scategs=[
+    SliceCateg {scname="Transport", scbg=green, scfg=white,
+      sctags=[TagTransport]},
+    SliceCateg {scname="Going Out", scbg=cyan, scfg=white,
+      sctags=[TagTravGoOut]},
+    SliceCateg {scname="Shopping", scbg=blue, scfg=white,
+      sctags=[TagTravShop]},
+    SliceCateg {scname="Housing", scbg=yellow, scfg=white,
+      sctags=[TagTravHous]}]},
+
+  Slice {sname="Income", stags=[TagIncome], scategs=[
+    SliceCateg {scname="Salary", scbg=green, scfg=white, sctags=[TagSalary]},
+    SliceCateg {scname="Discounts", scbg=blue,scfg=white,sctags=[TagDiscount]},
+    SliceCateg {scname="Other", scbg=grey, scfg=white, sctags=[TagOther]}]},
+
+  Slice {sname="Assets", stags=[TagAsset], scategs=[
+    SliceCateg {scname="Cash", scbg=red, scfg=white,
+      sctags=[TagCash]},
+    SliceCateg {scname="Shopping Credit", scbg=yellow, scfg=white,
+      sctags=[TagAmazon0097, TagAmzGiftAcc, TagVicSec1790]},
+    SliceCateg {scname="Checking and Debit", scbg=green, scfg=white,
+      sctags=[TagChecking1156, TagVisa7417, TagVisa9955]},
+    SliceCateg {scname="Savings", scbg=cyan, scfg=white,
+      sctags=[TagSavings0156]},
+    SliceCateg {scname="Credit Cards", scbg=blue, scfg=white,
+      sctags=[TagMaster6070, TagVisa4787]},
+    SliceCateg {scname="Other", scbg=grey, scfg=white,
+      sctags=[TagOther]}]},
+
+  Slice {sname="All", stags=[], scategs=[
+    SliceCateg {scname="Assets", scbg=green, scfg=white, sctags=[TagAsset]},
+    SliceCateg {scname="Income", scbg=blue, scfg=white, sctags=[TagIncome]},
+    SliceCateg {scname="Expenses",scbg=red,scfg=white,sctags=[TagExpense]}]}]
 
 instance Taggable (Bank, BankAcc, BankTrans) where
   tagged _ _ = False
@@ -30,11 +109,7 @@ instance Patchable Shop where
   patched = id
 
 instance Patchable Bank where
-  patched banks = banks ++ [Bank {bid="", baccs=[
-    BankAcc {baid="", balabel="", babalance=300, bacurrency=USD, bacard=False,
-             balimit=Nothing, bapaymin=Nothing, bapaytime=Nothing, batrans=[
-      BankTrans {btlabel="", btamount=100, bttime=1},
-      BankTrans {btlabel="", btamount=200, bttime=2}]}]}]
+  patched = id
 
 planfrom = 0
 planto = 0
--- hinance-www/www/examples/max/in/user_tag.hs
+++ hinance-www/www/examples/max/in/user_tag.hs
@@ -1,2 +1,12 @@
 module Hinance.User.Tag where
-data Tag = TagDummy deriving (Read, Show, Enum, Bounded, Eq, Ord)
+
+data Tag = TagAsset | TagExpense | TagIncome
+  -- Categories
+  | TagBooks | TagCar | TagCarInsur | TagCarMtn | TagCarPaper | TagClothes
+  | TagDiscount | TagDrugs | TagEatingOut | TagElectronics | TagEnergy
+  | TagFood | TagGames | TagGas | TagGoingOut | TagGrow | TagHobby
+  | TagHousehold | TagHygiene | TagInsurance | TagKitchen | TagMedSvc
+  | TagMovies | TagOther | TagOutdoor | TagParking | TagPhone | TagProjects
+  | TagRent | TagShipping | TagSalary | TagTax | TagTransport | TagTravel
+  | TagTravGoOut | TagTravHous | TagTravShop | TagWeight | TagYoga
+  deriving (Read, Show, Enum, Bounded, Eq, Ord)
