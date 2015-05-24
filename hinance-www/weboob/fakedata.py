0e69c713144ba8992a93ac4b550750792c9385cd
Oleg Plakhotniuk <olegus8@gmail.com>
2015-05-23 19:23:17 -0500

    examples

--- hinance-www/weboob/fakedata.py
+++ hinance-www/weboob/fakedata.py
@@ -334,7 +334,7 @@ for date in sample(list(datesrange((2012,7,10), (2015,5,1))), 5):
 # Payments for Windy Vault Bank credit card.
 seed(40726)
 for date in sample(list(datesrange((2012,11,1), (2015,5,1))), 10):
-  amount = Decimal(randint(300,1500))
+  amount = Decimal(randint(600,2000))
   visa8394.add(transaction(date=date, amount=amount,
     label=u'ONLINE PAYMENT'))
   checking1042.add(transaction(date=date, amount=-amount, label=choice([
@@ -355,7 +355,7 @@ for date in sample(list(datesrange((2013,12,1), (2015,5,1))), 5):
 # Online payments for Crispy Bills Bank credit card.
 seed(88798)
 for date in sample(list(datesrange((2013,12,1), (2015,5,1))), 15):
-  amount = Decimal(randint(300,2000))
+  amount = Decimal(randint(100,300))
   master8385.add(transaction(date=date, amount=amount,
     label=u'ONLINE PAYMENT, THANK YOU'))
   checking1042.add(transaction(date=date, amount=-amount, label=choice([
@@ -365,7 +365,7 @@ for date in sample(list(datesrange((2013,12,1), (2015,5,1))), 15):
 # Payments for Awesome Stuff store card.
 seed(97051)
 for date in sample(list(datesrange((2014,5,1), (2015,5,1))), 10):
-  amount = Decimal(randint(100,1000))
+  amount = Decimal(randint(10,100))
   awesome1875.add(transaction(date=date, amount=amount,
     label=u'ONLINE PAYMENT'))
   checking1042.add(transaction(date=date, amount=-amount, label=choice([
@@ -375,7 +375,7 @@ for date in sample(list(datesrange((2014,5,1), (2015,5,1))), 10):
 # Payments for Violently Gorgeous store card.
 seed(42732)
 for date in sample(list(datesrange((2014,2,1), (2015,5,1))), 15):
-  amount = Decimal(randint(50,500))
+  amount = Decimal(randint(5,50))
   viogor7260.add(transaction(date=date, amount=amount,
     label=u'PAYMENT - THANK YOU'))
   checking1042.add(transaction(date=date, amount=-amount, label=choice([
