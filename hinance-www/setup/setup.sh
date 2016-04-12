dadab6bca0cc7f61fae2ae234d057b5cff73d848
Oleg Plakhotniuk <olegus8@gmail.com>
2016-04-12 02:19:52 -0500

    npm

--- hinance-www/setup/setup.sh
+++ hinance-www/setup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pacman -S --noconfirm --needed imagemagick nodejs openssh
+pacman -S --noconfirm --needed imagemagick nodejs npm openssh
 chown -R user:user /hinance-www
 sudo -u user bash -l /hinance-www/setup/as-user.sh
 paccache -rk0
